30de1aef62cd20a1044b36ab07efe5eb6c16927a
OriSketchy <orisketchy@gmail.com>
2023-03-09 20:18:37 +0000

    removed unecessary scripts

--- Assets/WTT VR Experience/MyCharacterController.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class MyCharacterController : MonoBehaviour
-{
-    ///set up movement speed + snap turning  dont think we need anything else for this project
-}
